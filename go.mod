ec0864dc2d48421be0c0b0a0d2b04e876137bca4
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-09-15 01:29:10 -0300

    chore: add fatih/color

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/Gustavonobreza/go-watch
 
 go 1.17
 
+require github.com/fsnotify/fsnotify v1.5.1
+
 require (
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-func main() {
+func cls() {
+	print("\033[H\033[2J")
+}
+
+func banner() {
 	print(
 		`
 ░██████╗░░█████╗░░░░░░░░██╗░░░░░░░██╗░█████╗░████████╗░█████╗░██╗░░██╗
@@ -16,12 +24,74 @@ func main() {
 ╚██████╔╝╚█████╔╝░░░░░░░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░╚█████╔╝██║░░██║
 ░╚═════╝░░╚════╝░░░░░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
 `)
+}
+
+func printWaiting() {
+	println("Watching for changes...")
+}
+
+func printSetup() {
+	println("Preparing and starting...")
+}
+
+func main() {
+	cls()
+	banner()
+	printWaiting()
 	path, err := os.Getwd()
 	if err != nil {
+		print(path)
 		panic(err.Error())
+
 	}
-	// nodemon --exec "go build -o bin.exe $pathBase && $pathBase\bin.exe" --ignore "*.exe", "$path\.git\*" -e go
-	command := fmt.Sprintf(`nodemon --exec "go build -o bin.exe %v && %v\bin.exe" --ignore "*.exe", "%v\.git\*" -e go`, path, path, path)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	Run(path)
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				// log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					// log.Println("modified file:", event.Name)
+					separedByDot := strings.Split(event.Name, ".")
+					if separedByDot[len(separedByDot)-1] == "go" {
+						// Is a go file
+						cls()
+						banner()
+						printSetup()
+						Run(path)
+						printWaiting()
+					}
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+
+}
+func Run(path string) {
+	command := fmt.Sprintf(`go build -o bin.exe %v; %v\bin.exe`, path, path)
 	cmd := exec.Command("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", "-c", command)
 
 	cmd.Stdout = os.Stdout
